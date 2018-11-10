a511bdd15db1fd07919af06315340073c5133f53
PetiteArt <pawita1993@gmail.com>
2018-11-10 20:40:38 +0000

    added summation

--- src/PawitaGUI.java
+++ src/PawitaGUI.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
 
 public class PawitaGUI {
+
     private JFrame jFrame;
 
     public PawitaGUI() {
@@ -13,17 +14,30 @@ public class PawitaGUI {
     }
 
     public void showGUI() {
-        JButton b=new JButton("ME");
-        b.addActionListener(new ActionListener() {
+
+        JTextField box1 = new JTextField(20);
+        box1.setBounds(130,150,100, 40);
+
+
+        JTextField box2 = new JTextField(20);
+        box2.setBounds(130,200,100, 40);
+
+        JButton calculate = new JButton("Result");
+        calculate.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Yeah");
+                int int1 = Integer.parseInt(box1.getText());
+                int int2 = Integer.parseInt(box2.getText());
+                int answer = int1 + int2;
+                System.out.println(answer);
             }
         });//creating instance of JButton
-        b.setBounds(130,100,100, 40);
+        calculate.setBounds(130,300,100, 40);
 
         jFrame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        jFrame.add(b);//adding button in JFrame
+        jFrame.add(box1);
+        jFrame.add(box2);
+        jFrame.add(calculate);//adding button in JFrame
 
         jFrame.setSize(400,500);//400 width and 500 height
         jFrame.setLayout(null);//using no layout managers
